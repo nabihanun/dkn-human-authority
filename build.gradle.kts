6963f94bf139c2ae203e5cb5ecd70677d8ea6e62
nabihanun <musfiqrhelal@gmail.com>
2026-09-01 22:36:57 +0600

    Update lifecycle-viewmodel-compose dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,5 +20,5 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.8.5")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmo
 }

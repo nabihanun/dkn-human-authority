f5a95a1e6bd064ae758cde411ac14ca255ef95e8
nabihanun <musfiqrhelal@gmail.com>
2026-09-01 22:45:10 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
+    id("com.android.application") version "8.7.3"
+    id("org.jetbrains.kotlin.android") version "2.0.21"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
 }
+
 android {
     namespace = "com.dkn.humanauthority"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.dkn.humanauthority"
         minSdk = 24
@@ -14,11 +16,12 @@ android {
         versionName = "1.0"
     }
 }
+
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.activity:activity-compose:1.10.0")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.3.1")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.8.5")
-    implementation("androidx.lifecycle:lifecycle-viewmo
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
 }
